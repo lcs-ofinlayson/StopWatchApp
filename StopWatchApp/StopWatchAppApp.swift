d0dbfd0be387ec83b7e33a011ec9ff1d2e0459e5
Oliver Finlayson <ofinlayson@lcs.on.ca>
2022-11-09 09:23:57 -0500

    Initial Commit

---
+++ StopWatchApp/StopWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchAppApp.swift
+//  StopWatchApp
+//
+//  Created by Oliver Finlayson on 2022-11-09.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
